32f01f4f1328e5e1535663897540f63eb375306b
afrasiab058 <afrasiab_Swati@hotmail.com>
2015-11-09 07:30:49 +0500

    Branches Concept 2

---
+++ Branches/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Branches Concept");
+		int a=10;
+		int b=20;
+		int temp;
+		
+			temp=a+b;
+		System.out.println(temp);
+
+	}
+
+}
